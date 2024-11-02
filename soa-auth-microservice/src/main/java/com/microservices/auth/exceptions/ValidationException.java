b032c831bc0a9cab0ebfcef9eddc9481187b282d
teuzed <georguiazelpachecos@gmail.com>
2024-11-02 17:06:37 -0500

    [feat]: Added exception and client

---
+++ soa-auth-microservice/src/main/java/com/microservices/auth/exceptions/ValidationException.java
@@ -0,0 +1,13 @@
+package com.microservices.auth.exceptions;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+
+@Builder
+@Getter
+public class ValidationException extends RuntimeException {
+    private Map<String, String> validationErrors;
+}
