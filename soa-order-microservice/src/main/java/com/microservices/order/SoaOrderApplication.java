f703e3f1f29c58fef78b62dacb83cc6ec19d5556
teuzed <georguiazelpachecos@gmail.com>
2024-12-09 15:27:47 -0500

    [feat]: Added new service monitoring

--- soa-order-microservice/src/main/java/com/microservices/order/SoaOrderApplication.java
+++ soa-order-microservice/src/main/java/com/microservices/order/SoaOrderApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SoaOrderApplication {
@@ -12,4 +13,6 @@ public class SoaOrderApplication {
         SpringApplication.run(SoaOrderApplication.class, args);
     }
 
+
+ 
 }
